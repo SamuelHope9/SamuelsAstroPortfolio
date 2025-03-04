a0c27bbaf9dc40f46ad09d157bba844c64de942b
SamuelJamesFisher <64818521+SamuelJamesFisher@users.noreply.github.com>
2025-03-04 17:58:17 +1300

    Submit

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,29 +1,51 @@
 const CaptchaKey = import.meta.env.CAPTCHA_SECRET_API_KEY;
 
+import { string } from 'astro/zod';
 import { defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
 
+
+/*
+input: z.object({
+            name: z.string(),
+            email: z.string(),
+            message: z.string(),
+          }),
+
+*/
+
+
 export const server = {
     SubmitFormAction: defineAction({
-      accept: "form",
-      input: z.object({
-        name: z.string(),
-        email: z.string(),
-        message: z.string(),
-      }),
+        accept: 'form',
       handler: async (input) => {
-        console.log(`${z}!`)
 
+        console.log("SubmitForm action data: ");
+        console.log(input);
+        //const CaptchaResponse = String(token);
+        //const IsValidCaptcha = processCaptcha(CaptchaResponse)
 
+        //if(IsValidCaptcha)
+        //{
+         //   console.log("Valid ");
+        //}
+
+        return `Hello!`;
+
+        
+        //const IsValidCaptcha = await processCaptcha(CaptchaResponse);
         //const is_valid_captcha = await processCaptcha(Captcha)
+
         //if (is_valid_captcha)
         //{
             //const is_valid_captcha = await sendEmail(Captcha)
         //}
 
-        return `Hello, ${input.name}!`
+        //return `Hello, ${input.name}!`
       }
-    })};
+    })
+  };
+
 
 type formData = {
     name: string;
@@ -38,11 +60,11 @@ export async function processCaptcha(g_recaptcha_response: string) {
   
     const requestBody = new URLSearchParams({
       secret:
-        import.meta.env.RECAPTCHA_SECRET_KEY ||
         CaptchaKey,
       response: g_recaptcha_response
     })
-  
+    console.log("Try captcha");
+
     const response = await fetch(url, {
       method: 'POST',
       headers: {
@@ -52,7 +74,7 @@ export async function processCaptcha(g_recaptcha_response: string) {
     })
   
     const data = await response.json()
-  
+
     console.log(data)
     
     return data.success
@@ -63,10 +85,8 @@ export async function sendEmail(FormData)
     try {
         console.log("Test log");
     
-        const CaptchaResponse = FormData.get('g-recaptcha-response')?.toString();
-        const is_valid_captcha = await processCaptcha(CaptchaResponse);
-        /*
-        if (is_valid_captcha)
+        /* 
+        if (IsValidCaptcha)
         {
           
           const response = await fetch(Astro.url + "/api/sendEmail.json", {
